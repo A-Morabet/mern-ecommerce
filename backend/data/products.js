bed820915d07d8b6cc044f4ac096b653cc8ae741
CKDOutcast <morabet.amin@gmail.com>
2024-06-23 14:27:49 +0100

    started working on backend express server

---
+++ backend/data/products.js
@@ -0,0 +1,82 @@
+const products = [
+    {
+        _id: '1',
+        name: 'Airpods Wireless Bluetooth Headphones',
+        image: '/images/airpods.jpg',
+        description:
+            'Bluetooth BLABLA BLAB ALB BAAL aBLA ALB ABLAB ABLA LAB ABLAB ALB AALB ABLAB ALBA, ABLABLABLALBALBALBALB, ABLALBALB ABLALB aLBA bALBALBA aBL aBLABA aBLaBALBA',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: 89.99,
+        countInStock: 10,
+        rating: 4.5,
+        numReviews: 12,
+    },
+    {
+        _id: '2',
+        name: 'Alexa Cutting Edge',
+        image: '/images/alexa.jpg',
+        description:
+            'Bluetooth BLABLA BLAB ALB BAAL aBLA ALB ABLAB ABLA LAB ABLAB ALB AALB ABLAB ALBA, ABLABLABLALBALBALBALB, ABLALBALB ABLALB aLBA bALBALBA aBL aBLABA aBLaBALBA',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: 89.99,
+        countInStock: 10,
+        rating: 2.5,
+        numReviews: 12,
+    },
+    {
+        _id: '3',
+        name: 'Super Duper Mouse',
+        image: '/images/mouse.jpg',
+        description:
+            'Bluetooth BLABLA BLAB ALB BAAL aBLA ALB ABLAB ABLA LAB ABLAB ALB AALB ABLAB ALBA, ABLABLABLALBALBALBALB, ABLALBALB ABLALB aLBA bALBALBA aBL aBLABA aBLaBALBA',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: 89.99,
+        countInStock: 10,
+        rating: 4.5,
+        numReviews: 12,
+    },
+    {
+        _id: '4',
+        name: `It's a lie!!!!`,
+        image: '/images/playstation.jpg',
+        description:
+            'Bluetooth BLABLA BLAB ALB BAAL aBLA ALB ABLAB ABLA LAB ABLAB ALB AALB ABLAB ALBA, ABLABLABLALBALBALBALB, ABLALBALB ABLALB aLBA bALBALBA aBL aBLABA aBLaBALBA',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: 89.99,
+        countInStock: 0,
+        rating: 1,
+        numReviews: 12,
+    },
+    {
+        _id: '5',
+        name: 'Alexa Cutting Edge',
+        image: '/images/alexa.jpg',
+        description:
+            'Bluetooth BLABLA BLAB ALB BAAL aBLA ALB ABLAB ABLA LAB ABLAB ALB AALB ABLAB ALBA, ABLABLABLALBALBALBALB, ABLALBALB ABLALB aLBA bALBALBA aBL aBLABA aBLaBALBA',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: 89.99,
+        countInStock: 10,
+        rating: 4.5,
+        numReviews: 12,
+    },
+    {
+        _id: '6',
+        name: 'Super Duper Mouse',
+        image: '/images/mouse.jpg',
+        description:
+            'Bluetooth BLABLA BLAB ALB BAAL aBLA ALB ABLAB ABLA LAB ABLAB ALB AALB ABLAB ALBA, ABLABLABLALBALBALBALB, ABLALBALB ABLALB aLBA bALBALBA aBL aBLABA aBLaBALBA',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: 89.99,
+        countInStock: 10,
+        rating: 4.5,
+        numReviews: 12,
+    },
+]
+
+export default products;
