d274cdf132071786916cda4d21ab09d67d2a3fc0
CKDOutcast <morabet.amin@gmail.com>
2024-06-27 15:11:48 +0100

    added data seeder and scripts for data import and destroy

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -49,7 +49,7 @@ const productSchema = new mongoose.Schema({
     },
     reviews: [reviewSchema],
     rating: {
-        type: number,
+        type: Number,
         required: true,
         default: 0,
     },
