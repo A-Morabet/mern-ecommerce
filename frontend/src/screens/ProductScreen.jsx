79a866e31a4eae38633f7f83ef0f1d04537941fa
CKDOutcast <morabet.amin@gmail.com>
2024-07-02 20:21:30 +0100

    added spinner and proper error display

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem} from 'react-bootstrap'
 import Rating from '../components/Rating'
 import { useGetProductDetailsQuery } from '../slices/productsApiSlice.js'
+import Loader from '../components/Loader.jsx'
+import Message from '../components/Message.jsx'
 
 
 
@@ -20,7 +22,7 @@ function ProductScreen() {
     <>
     <Link className='btn btn-light my-3' to="/" >Go Back</Link>
 
-    { isLoading ? (<h2>Loading...</h2>) : error ? (<div>error?.data?.message || error.error </div>) : (
+    { isLoading ? (<Loader/>) : error ? (<Message variant = 'danger'>{ error?.data?.message  || error.error }</Message>) : (
         <Row>
         <Col md={5}>
         <Image src={product.image} alt={product.name} fluid />
