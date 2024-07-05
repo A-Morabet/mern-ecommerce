f115aec835e9ce9ceb03709a761a55e7ebe64ce4
CKDOutcast <morabet.amin@gmail.com>
2024-07-05 18:06:47 +0100

    introduced initial cart screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,6 +9,7 @@ import './assets/styles/index.css';
 import App from './App';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 import reportWebVitals from './reportWebVitals';
 
 const router = createBrowserRouter(
@@ -16,6 +17,7 @@ const router = createBrowserRouter(
     <Route path ="/" element={<App />} >
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
       </Route>
   )
 )
---
+++ frontend/src/screens/CartScreen.jsx
@@ -0,0 +1,88 @@
+import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, ListGroup, Image, Form, Button, Card, ListGroupItem } from 'react-bootstrap'
+import { FaTrash } from 'react-icons/fa';
+import Message from '../components/Message'
+import { addToCart } from '../slices/cartSlice.js';
+
+function CartScreen() {
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const cart = useSelector((state) => state.cart)
+    const { cartItems } = cart
+
+    async function addToCartHandler(product, qty) {
+        dispatch(addToCart({...product, qty}))
+    }
+
+  return (
+    <Row>
+        <Col md={8}>
+        <h1 style={{ marginBottom: '20px'}}>Shopping Cart</h1>
+        { cartItems.length === 0 ? (
+            <Message>
+                Your cart is empty <Link to='/'>Go Back</Link>
+            </Message>
+        ) : (
+            <ListGroup variant='flush'>
+                { cartItems.map((item) => (
+                    <ListGroupItem key = {item._id}>
+                        <Row>
+                            <Col md={2}>
+                            <Image src={item.image} alt={item.name} fluid rounded />
+                            </Col>
+                            <Col md={3}>
+                            <Link to={`/product/${item._id}`}>{item.name}</Link>
+                            </Col>
+                            <Col md={2}>
+                            ${item.price}
+                            </Col>
+                            <Col md={2}>
+                            <Form.Control
+                            as = 'select'
+                            value = {item.qty}
+                            onChange = {(e) => addToCartHandler(item, Number(e.target.value))} 
+                            >
+                            { [...Array(item.countInStock).keys()].map((ele) => (
+                                <option key={ ele + 1} value = {ele + 1}>
+                                    { ele + 1}
+                                </option>
+                            )) }
+                            </Form.Control>
+                            </Col>
+                            <Col md={2}>
+                            <Button type='button' variant='light'>
+                                <FaTrash/>
+                            </Button>
+                            </Col>
+                          
+                        </Row>
+                    </ListGroupItem>
+                ))} 
+            </ListGroup>
+        )}
+        </Col>
+        <Col md={4}>
+            <Card >
+            <ListGroup variant='flush'>
+                <ListGroup.Item>
+                    <h2>
+                        SubTotal ({ cartItems.reduce((acc, item) => acc + item.qty, 0)}) items
+                    </h2>
+                    ${ cartItems.reduce((acc, item) => acc + item.qty * item.price, 0 ).toFixed(2)}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                <Button type='button' className='btn-block' disabled={ cartItems.length === 0}>
+                    Proceed to Checkout
+                </Button>
+                </ListGroup.Item>
+            </ListGroup>
+            </Card>
+            </Col>
+    </Row>
+  )
+}
+
+export default CartScreen
