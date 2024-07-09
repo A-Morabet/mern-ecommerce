3a6135c5b31212093e4dfc822b4eaf51d6172ca9
CKDOutcast <morabet.amin@gmail.com>
2024-07-09 12:12:53 +0100

    added logout request in user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -59,8 +59,14 @@ const registerUser = asyncHandler (async (req,res) => {
 
 const logoutUser = asyncHandler (async (req,res) => {
     
-    res.send('logout user')
- 
+    res.cookie('jwt', '', {
+      httpOnly: true,
+      expires: new Date(0)
+   })
+   
+   res.status(200).json({
+      message: 'Logged out successfully'
+   })
  })
 
 // @desc  Get user profile
