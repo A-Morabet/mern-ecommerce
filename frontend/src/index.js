4dabdab96d460c6e12f345fbbaa1a1675878a948
CKDOutcast <morabet.amin@gmail.com>
2024-07-14 12:02:57 +0100

    added frontend register functionality

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,6 +11,7 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 import reportWebVitals from './reportWebVitals';
 
 const router = createBrowserRouter(
@@ -20,6 +21,7 @@ const router = createBrowserRouter(
       <Route path="/product/:id" element={<ProductScreen />} />
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
       </Route>
   )
 )
