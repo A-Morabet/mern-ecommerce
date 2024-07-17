2a19907f1a19cd7b9f4567bf2e4c7fae185df345
CKDOutcast <morabet.amin@gmail.com>
2024-07-17 19:10:52 +0100

    added private route to shipping route

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,6 +14,7 @@ import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import reportWebVitals from './reportWebVitals';
+import PrivateRoute from './components/PrivateRoute.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -23,7 +24,11 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
+      
+
+      <Route path ='' element={<PrivateRoute />}>
       <Route path="/shipping" element={<ShippingScreen />} />
+      </Route>
       </Route>
   )
 )
