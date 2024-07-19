f7b79162f7e56edd3dd4a31e2d1b3cdb7c58abf4
CKDOutcast <morabet.amin@gmail.com>
2024-07-19 19:24:10 +0100

    added payment method screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,8 +13,9 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
 import reportWebVitals from './reportWebVitals';
-import PrivateRoute from './components/PrivateRoute.jsx';
+import PrivateRoute from './components/PrivateRoute';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -28,6 +29,7 @@ const router = createBrowserRouter(
 
       <Route path ='' element={<PrivateRoute />}>
       <Route path="/shipping" element={<ShippingScreen />} />
+      <Route path="/payment" element={<PaymentScreen />} />
       </Route>
       </Route>
   )
