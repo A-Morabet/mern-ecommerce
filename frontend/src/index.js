5a7c1ffdb94c59017fcb41955c3686f9cb9c84ff
CKDOutcast <morabet.amin@gmail.com>
2024-07-20 22:39:38 +0100

    added place order functionality on frontend by expanding controller and creating api slice and screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,6 +14,7 @@ import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import reportWebVitals from './reportWebVitals';
 import PrivateRoute from './components/PrivateRoute';
 
@@ -30,6 +31,7 @@ const router = createBrowserRouter(
       <Route path ='' element={<PrivateRoute />}>
       <Route path="/shipping" element={<ShippingScreen />} />
       <Route path="/payment" element={<PaymentScreen />} />
+      <Route path="/placeorder" element={<PlaceOrderScreen />} />
       </Route>
       </Route>
   )
