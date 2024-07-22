69f1ed2b142895265aef4eecec409fcdb3dbfd7c
CKDOutcast <morabet.amin@gmail.com>
2024-07-22 14:26:32 +0100

    added order screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,6 +15,7 @@ import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 import reportWebVitals from './reportWebVitals';
 import PrivateRoute from './components/PrivateRoute';
 
@@ -32,6 +33,7 @@ const router = createBrowserRouter(
       <Route path="/shipping" element={<ShippingScreen />} />
       <Route path="/payment" element={<PaymentScreen />} />
       <Route path="/placeorder" element={<PlaceOrderScreen />} />
+      <Route path="/order/:id" element={<OrderScreen />} />
       </Route>
       </Route>
   )
