bb9f6c3db7287932dcdedf4c995b67bd72180ad2
CKDOutcast <morabet.amin@gmail.com>
2024-07-23 17:52:33 +0100

    added paypal env id variable for testing

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -79,13 +79,31 @@ const getOrderById = asyncHandler (async (req,res) => {
 
 const updateOrderToPaid  = asyncHandler (async (req,res) => {
     
-    res.send('update order to be paid')
+    const order = await Order.findById(req.params.id)
 
+    if (order) {
+        order.isPaid = true
+        order.paidAit = Date.now()
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address,
+        }
+    
+
+    const updatedOrder = await order.save()
+
+    res.status(200).json(updatedOrder)
+    } else {
+        res.statuts(404)
+        throw new Error('Order not found')
+    }
 })
 
 
 // @desc    Update order to delivered
-// @route   GET /api/orders/:id/deliver
+// @route   PUT /api/orders/:id/deliver
 // @access  Private/Admin
 
 const updateOrderToDelivered  = asyncHandler (async (req,res) => {
