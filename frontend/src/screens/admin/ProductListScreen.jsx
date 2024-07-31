66710e6ca8403f29e69ba7b6e80d9a500ad44615
CKDOutcast <morabet.amin@gmail.com>
2024-07-31 21:07:08 +0100

    added product edit screen for admin (still wip)

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,6 +1,6 @@
 import { LinkContainer } from 'react-router-bootstrap'
 import { Table, Button, Row, Col } from 'react-bootstrap'
-import { FaTimes, FaEdit, FaTrash } from 'react-icons/fa'
+import { FaEdit, FaTrash } from 'react-icons/fa'
 import Message from '../../components/Message'
 import Loader from '../../components/Loader'
 import { toast } from 'react-toastify'
@@ -63,8 +63,8 @@ function ProductListScreen() {
                         <td>{product._id}</td>
                         <td>{product.name}</td>
                         <td>${product.price}</td>
-                        <td>{product._category}</td>
-                        <td>{product._brand}</td>
+                        <td>{product.category}</td>
+                        <td>{product.brand}</td>
                         <td>
                         <LinkContainer to = {`/admin/product/${product._id}/edit`}>
                         <Button variant='light' className='btn-sm mx-2'>
