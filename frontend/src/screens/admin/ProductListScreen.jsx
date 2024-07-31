ae860bc9519157215c91edbe4a86880b33ff23ff
CKDOutcast <morabet.amin@gmail.com>
2024-07-31 20:16:55 +0100

    added create product functionality (still wip)

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -3,16 +3,30 @@ import { Table, Button, Row, Col } from 'react-bootstrap'
 import { FaTimes, FaEdit, FaTrash } from 'react-icons/fa'
 import Message from '../../components/Message'
 import Loader from '../../components/Loader'
-import { useGetProductsQuery } from '../../slices/productsApiSlice.js'
+import { toast } from 'react-toastify'
+import { useGetProductsQuery, useCreateProductMutation } from '../../slices/productsApiSlice.js'
 
 function ProductListScreen() {
 
-    const { data: products, isLoading, error } = useGetProductsQuery()
+    const { data: products, isLoading, error, refetch } = useGetProductsQuery()
+
+    const [createProduct, { isLoading: loadingCreate }] = useCreateProductMutation()
 
     function deleteHandler(id) {
         console.log('delete ' + id)
     }
 
+    async function createProductHandler() {
+        if (window.confirm('Are you sure you want to create a new product?')) {
+            try {
+                await createProduct()
+                refetch()
+            } catch (err) {
+                toast.error(err?.data?.message || err.error)
+            }
+
+        }
+    }
 
   return (
     <>
@@ -21,11 +35,13 @@ function ProductListScreen() {
         <h1>Products</h1>
         </Col>
         <Col className='text-end'>
-        <Button className='btn-sm m-3'>
+        <Button className='btn-sm m-3' onClick={ createProductHandler }>
             <FaEdit /> Create Product
         </Button>
         </Col>
     </Row>
+
+    {loadingCreate && <Loader />}
     { isLoading? <Loader/> : error ? <Message variant='danger'>
         {error}
     </Message> : (
@@ -46,7 +62,7 @@ function ProductListScreen() {
                     <tr key={product._id}>
                         <td>{product._id}</td>
                         <td>{product.name}</td>
-                        <td>{product.price}</td>
+                        <td>${product.price}</td>
                         <td>{product._category}</td>
                         <td>{product._brand}</td>
                         <td>
