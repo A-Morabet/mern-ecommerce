d3f303205316e7224f608b286ef71279bf6c51b0
CKDOutcast <morabet.amin@gmail.com>
2024-10-27 12:39:24 +0100

    introduced pinging algorithm for 10 minutes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -27,3 +27,19 @@ root.render(
 
 
 reportWebVitals();
+
+const url = `https://greenasaur.onrender.com//`; // Replace with your Render URL
+const interval = 600000; // Interval in milliseconds (30 seconds)
+
+//Reloader Function
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+setInterval(reloadWebsite, interval);
